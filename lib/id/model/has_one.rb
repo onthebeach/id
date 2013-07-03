36df228b3eb89262074670bd6e30a436cba6b35f
Radek Molenda <radek.molenda@onthebeach.co.uk>
2013-07-03 14:35:46 +0100

    Don't use non existing variable references

--- lib/id/model/has_one.rb
+++ lib/id/model/has_one.rb
@@ -4,8 +4,8 @@ module Id
 
       def value_of(data)
         if optional?
-          child = data.fetch(field.key, nil)
-          child.nil? ? None : Some[field.type.new(child)]
+          child = data.fetch(key, nil)
+          child.nil? ? None : Some[type.new(child)]
         else
           child = data.fetch(key) { raise MissingAttributeError, key }
           type.new(child) unless child.nil?
